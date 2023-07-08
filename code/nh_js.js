8dbaf02b851f49ca8c51c2d69f737adbe0c64dec
N1kkoin <nikkonh@hotmail.com>
2023-07-08 12:53:17 -0300

    arrumei href gambiarra

--- code/nh_js.js
+++ code/nh_js.js
@@ -1,45 +1,39 @@
-function escrevendoNaHome() {
-    const phrases = ["Designer.", "Ilustradora.", "Criativa.", "Nikkoin Design"];
-    const typeDelay = 100; // delay between typing each character in ms
-    const deleteDelay = 50; // delay before deleting each character in ms
-    const nextPhraseDelay = 2000; // delay before starting to type the next phrase in ms
-    const loopDelay = 500; // delay between loops in ms
+const phrases = ["Designer.", "Ilustradora.", "Criativa.", "Nikkoin Design"];
+const typeDelay = 100; // delay between typing each character in ms
+const deleteDelay = 50; // delay before deleting each character in ms
+const nextPhraseDelay = 2000; // delay before starting to type the next phrase in ms
+const loopDelay = 500; // delay between loops in ms
 
-    let currentPhraseIndex = 0;
-    let isDeleting = false;
-    let currentText = "";
-    let typeTimeoutId = null;
+let currentPhraseIndex = 0;
+let isDeleting = false;
+let currentText = "";
+let typeTimeoutId = null;
 
-    function type() {
-        const currentPhrase = phrases[currentPhraseIndex];
-        currentText = isDeleting
-        ? currentPhrase.substring(0, currentText.length - 1)
-        : currentPhrase.substring(0, currentText.length + 1);
-        document.getElementById("typewriter").textContent = currentText;
+function type() {
+    const currentPhrase = phrases[currentPhraseIndex];
+    currentText = isDeleting
+    ? currentPhrase.substring(0, currentText.length - 1)
+    : currentPhrase.substring(0, currentText.length + 1);
+    document.getElementById("typewriter").textContent = currentText;
 
-        if (!isDeleting && currentText === currentPhrase) {
-        isDeleting = true;
-        typeTimeoutId = setTimeout(() => {
-            type();
-        }, nextPhraseDelay);
-        } else if (isDeleting && currentText === "") {
-        isDeleting = false;
-        currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
-        typeTimeoutId = setTimeout(() => {
-            type();
-        }, loopDelay);
-        } else {
-        typeTimeoutId = setTimeout(() => {
-            type();
-        }, isDeleting ? deleteDelay : typeDelay);
-        }
+    if (!isDeleting && currentText === currentPhrase) {
+    isDeleting = true;
+    typeTimeoutId = setTimeout(() => {
+        type();
+    }, nextPhraseDelay);
+    } else if (isDeleting && currentText === "") {
+    isDeleting = false;
+    currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+    typeTimeoutId = setTimeout(() => {
+        type();
+    }, loopDelay);
+    } else {
+    typeTimeoutId = setTimeout(() => {
+        type();
+    }, isDeleting ? deleteDelay : typeDelay);
     }
-
-    type();
 }
-function hoverproblemamobile() {
-
-}
+type();
 
 
 
