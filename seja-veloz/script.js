2eaa7c8b41fde1e9609f90d1e9bfaac97c50e3d4
N1kkoin <nikkonh@hotmail.com>
2024-09-06 00:37:30 -0300

    att

--- seja-veloz/script.js
+++ seja-veloz/script.js
@@ -39,3 +39,39 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     };
   });
+
+  // overlay --------------------------------------------------------------------------------------------------~
+
+  // Seleciona todas as imagens e o overlay
+const images = document.querySelectorAll('.imgs-paraovelay');
+const overlay = document.getElementById('overlay');
+const overlayImg = document.getElementById('overlay-img');
+const closeBtn = document.querySelector('.close');
+const body = document.body;
+
+
+// Quando uma imagem é clicada
+images.forEach(image => {
+  image.addEventListener('click', function() {
+    overlay.style.display = 'flex';  // Mostra o overlay
+    overlayImg.src = this.src;       // Define o src da imagem clicada no overlay
+    body.classList.add('no-scroll'); // Adiciona a classe que desabilita o scroll
+
+  });
+});
+
+// Quando o botão de fechar é clicado
+closeBtn.addEventListener('click', function() {
+  overlay.style.display = 'none'; // Esconde o overlay
+  body.classList.remove('no-scroll'); // Remove a classe para habilitar o scroll
+
+});
+
+// Fecha o overlay ao clicar fora da imagem
+overlay.addEventListener('click', function(event) {
+  if (event.target !== overlayImg) {
+    overlay.style.display = 'none'; // Esconde o overlay
+    body.classList.remove('no-scroll'); // Remove a classe para habilitar o scroll
+
+  }
+});
