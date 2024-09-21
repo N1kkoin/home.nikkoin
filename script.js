1ed414616d827545191c2bb59924578d49a15e39
N1kkoin <nikkonh@hotmail.com>
2024-09-21 17:55:35 -0300

    começando novo site no hostinger/ new design

---
+++ script.js
@@ -0,0 +1,48 @@
+const phrases = ["Web Designer,", "Ilustradora,", "Front-end,", "Criativa,", "Designer,", "Nikkoin Design,", "Nicole H."];
+const typeDelay = 100; // delay between typing each character in ms
+const deleteDelay = 50; // delay before deleting each character in ms
+const nextPhraseDelay = 2000; // delay before starting to type the next phrase in ms
+const loopDelay = 200; // delay between loops in ms
+
+let currentPhraseIndex = 0;
+let isDeleting = false;
+let currentText = "";
+let typeTimeoutId = null;
+
+function type() {
+    const currentPhrase = phrases[currentPhraseIndex];
+    currentText = isDeleting
+    ? currentPhrase.substring(0, currentText.length - 1)
+    : currentPhrase.substring(0, currentText.length + 1);
+    document.getElementById("typewriter").textContent = currentText;
+
+    if (!isDeleting && currentText === currentPhrase) {
+    isDeleting = true;
+    typeTimeoutId = setTimeout(() => {
+        type();
+    }, nextPhraseDelay);
+    } else if (isDeleting && currentText === "") {
+    isDeleting = false;
+    currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+    typeTimeoutId = setTimeout(() => {
+        type();
+    }, loopDelay);
+    } else {
+    typeTimeoutId = setTimeout(() => {
+        type();
+    }, isDeleting ? deleteDelay : typeDelay);
+    }
+}
+type();
+
+
+// NAVBAR //////////////////////////////////////////////////////////////////
+
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 600) {
+        nav.classList.add('scrolled-past-600');
+    } else {
+        nav.classList.remove('scrolled-past-600');
+    }
+});
