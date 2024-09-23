df0bece9187c44cec10b5b3a355a4a12309fc15e
N1kkoin <nikkonh@hotmail.com>
2024-09-23 16:42:07 -0300

    att

---
+++ 404-page.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nikkoin Design</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="alternate icon" href="images/icone.ico">
+    <link rel="icon" href="images/icone.svg">
+</head>
+
+<body>
+    <?php include 'code/navbar.php'; ?>
+
+    <div >
+      
+
+        <main class="quatrozeroquatro_main">
+404 ERROR
+        </main>
+ 
+
+        <?php include 'code/footer.php'; ?>
+     
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
