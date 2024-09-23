cdc78c062a6f2f9e7c11b9b02a633b0b15b6e707
N1kkoin <nikkonh@hotmail.com>
2024-09-23 15:06:07 -0300

    Update navbar.php

--- code/navbar.php
+++ code/navbar.php
@@ -2,8 +2,8 @@
     <div class="nav_container">
         <a target="_blank" href="#" class="logo"><img src="images/nh_logo.svg"></a>
         <div class="nav_links">
-            <a href="">Inicio</a>
-            <a href="">Portifólio</a>
+            <a href="#">Inicio</a>
+            <a href="/portifolio">Portifólio</a>
             <a target="_blank" href="/seja-veloz">Seja Veloz</a>
         </div>
     </div>
