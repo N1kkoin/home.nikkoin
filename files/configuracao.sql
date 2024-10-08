05e3dd2620e9a76cad5ea922e40a56b10d0e24d1
N1kkoin <nikkonh@hotmail.com>
2024-10-07 22:51:38 -0300

    remove files.nikkoin | agora no seu proprio repo

--- files/configuracao.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `username` varchar(50) NOT NULL,
- `email` varchar(50) NOT NULL,
- `password` varchar(255) NOT NULL, -- Aumentando para 255 para suportar senhas hash
- `create_datetime` datetime NOT NULL,
- `role` ENUM('admin', 'user') DEFAULT 'user', -- Novo campo de função
- PRIMARY KEY (`id`)
-);
--- files/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    // Destroy session
-    if(session_destroy()) {
-        // Redirecting To Home Page
-        header("Location: /");
-        exit(); // Certifique-se de sempre usar exit() após o redirecionamento
-    }
-?>
