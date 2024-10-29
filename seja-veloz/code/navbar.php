75f950b79ee9b12a3635a78afd9826a5a28963e3
N1kkoin <nikkonh@hotmail.com>
2024-10-28 22:04:58 -0300

    Update navbar.php

--- seja-veloz/code/navbar.php
+++ seja-veloz/code/navbar.php
@@ -8,7 +8,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo isset($title) ? $title : 'Seja Veloz'; ?></title>
-  <link rel="stylesheet" href="/seja-veloz/style.css?v=1.5">
+  <link rel="stylesheet" href="/seja-veloz/style.css?v=1.6">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 </head>
